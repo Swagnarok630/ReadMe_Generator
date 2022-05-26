1e26b41c91857141390c95a11ecc01d2ec16a488
Swagnarok <darryl.le.630@gmail.com>
2022-05-25 18:35:16 -0700

    updated questions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-testingDope Stuff
+
+    # Scooby doo
+    we solve mysteries
+    App can be found deployed [here](https://www.scooby.com).
+
+    ## Installation Instructions
+    get into the mystery machine
+    
--- index.js
+++ index.js
@@ -6,23 +6,45 @@ const questions = [
         type: "input",
         name: "title",
         message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install your project?",
+    },
+    {
+        type: "input",
+        name: "deployed",
+        message: "What is the link to your deployed project?",
     }
 ]
 
-function createReadme(input) {
-    fs.writeFileSync("./README.md", "testing " + input)
+function createReadme(answers) {
+    fs.writeFileSync("./README.md", `
+    # ${answers.title}
+    ${answers.description}
+    App can be found deployed [here](${answers.deployed}).
+
+    ## Installation Instructions
+    ${answers.installation}
+    `)
 }
 
 inquirer
 .prompt(questions)
 .then((answers) => {
-    createReadme(answers.title)
+    createReadme(answers)
 })
 .catch((error) => {
-    if (error.uhoh) {
-        console.error("Whoopsie!")
+    if (error.isTtyError) {
+        console.error("Could not render in current environment.")
     } else {
-        console.error("Something went wrong!", error)
+        console.error("Something else went wrong.", error)
     }
 });
 
